2f79a341bd77a5df13ec96d4ed921a6cb56d811d
bundumpling <bunyourdumplings@gmail.com>
2021-08-06 14:54:48 -0400

    Increased mock data size

--- src/seed.js
+++ src/seed.js
@@ -1,21 +1,258 @@
+import { triggerRef } from "vue";
 
 export const seedData = [
-    {
-      name: "umaruCry",
-      animated: false,
-      count: 2,
-      type: "FFZ"
-    },
-    {
-      name: "bundumHappy",
-      animated: false,
-      count: 5,
-      type: "Twitch"
-    },
-    {
-      name: "BLANKIES",
-      animated: true,
-      count: 22,
-      type: "BTTV"
-    }
-  ];
+  {
+    name: "bundumCry",
+    animated: false,
+    count: 5,
+    type: "Twitch"
+  },
+  {
+    name: "bundumHappy",
+    animated: false,
+    count: 5,
+    type: "Twitch"
+  },
+  {
+    name: "bundumLove",
+    animated: false,
+    count: 5,
+    type: "Twitch"
+  },
+  {
+    name: "bundumMad",
+    animated: false,
+    count: 5,
+    type: "Twitch"
+  },
+  {
+    name: "bundumPregyoza",
+    animated: false,
+    count: 5,
+    type: "Twitch"
+  },
+  {
+    name: "bundumZzz",
+    animated: false,
+    count: 5,
+    type: "Twitch"
+  },
+  {
+    name: "bundumpCry",
+    animated: false,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "bundumpDed",
+    animated: false,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "bundumpHappy",
+    animated: false,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "bundumpLove",
+    animated: false,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "bundumpMad",
+    animated: false,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "bundumpShock",
+    animated: false,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "bundumpWeird",
+    animated: false,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "bundumpWorry",
+    animated: false,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "bundumpZzz",
+    animated: false,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "birbThinking",
+    animated: false,
+    count: 0,
+    type: "FFZ"
+  },
+  {
+    name: "BocchiHands",
+    animated: false,
+    count: 0,
+    type: "FFZ"
+  },
+  {
+    name: "catoLove",
+    animated: false,
+    count: 0,
+    type: "FFZ"
+  },
+  
+  {
+    name: "catoOoh",
+    animated: false,
+    count: 0,
+    type: "FFZ"
+  },
+  {
+    name: "catoSlepo",
+    animated: false,
+    count: 0,
+    type: "FFZ"
+  },
+  {
+    name: "FeelsGulagMan",
+    animated: false,
+    count: 0,
+    type: "FFZ"
+  },
+  {
+    name: "OhIReallySee",
+    animated: false,
+    count: 0,
+    type: "FFZ"
+  },
+  {
+    name: "PEPW",
+    animated: false,
+    count: 0,
+    type: "FFZ"
+  },
+  {
+    name: "POGGERS",
+    animated: false,
+    count: 0,
+    type: "FFZ"
+  },
+  {
+    name: "SadCat",
+    animated: false,
+    count: 0,
+    type: "FFZ"
+  },
+  {
+    name: "umaruCry",
+    animated: false,
+    count: 2,
+    type: "FFZ"
+  },
+  {
+    name: "widepeepoHappy",
+    animated: false,
+    count: 0,
+    type: "FFZ"
+  },
+  {
+    name: "Wowega",
+    animated: false,
+    count: 0,
+    type: "FFZ"
+  },
+  {
+    name: "BLANKIES",
+    animated: true,
+    count: 22,
+    type: "BTTV"
+  },
+  {
+    name: "HAACHAMA",
+    animated: true,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "modCheck",
+    animated: true,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "MYAAA",
+    animated: true,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "NitoriNom",
+    animated: true,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "NODDERS",
+    animated: true,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "NOPERS",
+    animated: true,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "NOTED",
+    animated: true,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "peepersD",
+    animated: true,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "peepoD",
+    animated: true,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "peepoT",
+    animated: true,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "ReallyConcerned",
+    animated: true,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "TANTIES",
+    animated: true,
+    count: 0,
+    type: "BTTV"
+  },
+  {
+    name: "UMARUOMEGACRY",
+    animated: true,
+    count: 0,
+    type: "BTTV"
+  },
+  
+];
