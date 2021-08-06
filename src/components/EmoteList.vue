68fd4ffae64c2f16f0a325387fbffd594a03aab0
bundumpling <bunyourdumplings@gmail.com>
2021-08-06 19:03:54 -0400

    Removed unused state and computed from EmoteList

--- src/components/EmoteList.vue
+++ src/components/EmoteList.vue
@@ -12,25 +12,9 @@
 
 <script>
 import EmoteListItem from "./EmoteListItem.vue";
-import { store } from "../store.js";
 
 export default {
   name: "EmoteList",
-  data() {
-    return {
-      sharedState: store.state,
-    };
-  },
-  computed: {
-    randomizedCounts() {
-      return this.sharedState.seedData
-        .map((e) => {
-          e.count = Math.floor(Math.random() * 10000);
-          return e;
-        })
-        .sort((a, b) => b.count - a.count);
-    },
-  },
   props: ["emoteListType", "emoteList"],
   components: {
     EmoteListItem,
