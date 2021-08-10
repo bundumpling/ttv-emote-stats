7e81b40796d5d48dcfc02304470fecd9e4199b98
bundumpling <bunyourdumplings@gmail.com>
2021-08-10 14:47:13 -0400

    add SettingsSelectChannel component

---
+++ src/components/SettingsSelectChannel.vue
@@ -0,0 +1,22 @@
+<template>
+  <span class="channel-name"
+    >Channel: {{ channelName }} (TwitchID: {{ channelTwitchID }})</span
+  >
+</template>
+
+<script>
+export default {
+  name: "SettingsSelectChannel",
+  props: {
+    channelName: String,
+    channelTwitchID: Number,
+  },
+};
+</script>
+
+<style>
+.channel-name {
+  font-size: 1.5em;
+  font-family: monospace;
+}
+</style>
--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -1,16 +1,36 @@
 <template>
   <TheHeader />
   <Subheader msg="Settings" />
-  <EmoteListOptionsPanel />
+  <div class="container">
+    <SettingsSelectChannel
+      :channelName="channelName"
+      :channelTwitchID="channelTwitchID"
+    />
+    <EmoteListOptionsPanel />
+  </div>
 </template>
 
 <script>
 import TheHeader from "../components/TheHeader.vue";
 import Subheader from "../components/Subheader.vue";
+import SettingsSelectChannel from "../components/SettingsSelectChannel.vue";
 import EmoteListOptionsPanel from "../components/EmoteListOptionsPanel.vue";
+import { store } from "../store";
+
 export default {
   name: "SettingsPage",
-  components: { TheHeader, Subheader, EmoteListOptionsPanel },
+  data() {
+    return {
+      channelName: store.state.channel.name,
+      channelTwitchID: store.state.channel.twitchID,
+    };
+  },
+  components: {
+    TheHeader,
+    Subheader,
+    SettingsSelectChannel,
+    EmoteListOptionsPanel,
+  },
 };
 </script>
 
