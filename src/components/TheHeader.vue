f1781781c29fb911ed249f17babe1c78a441550f
bundumpling <bunyourdumplings@gmail.com>
2021-08-09 23:31:18 -0400

    Add TheHeader with new logo

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <figure class="container" role="img" aria-labelledby="TTV EMOTE STATS LOGO">
+    <img
+      src="/img/logo.png"
+      class="ascii-art-banner"
+      alt="ASCII block art logo for TTV Emote Stats"
+    />
+  </figure>
+  <nav>
+    <a href="/Rankings"><span>Rankings</span></a> |
+    <a href="/Settings"><span>Settings</span></a>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "TheHeader",
+};
+</script>
+
+<style>
+</style>
