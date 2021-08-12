9a44790a352c03d8f88d958abdaccb7f0e56b590
bundumpling <bunyourdumplings@gmail.com>
2021-08-11 22:49:42 -0400

    add flexbox styling

--- src/components/SettingsEmoteAPIControl.vue
+++ src/components/SettingsEmoteAPIControl.vue
@@ -1,21 +1,19 @@
 <template>
   <div class="column emote-api-control">
     <div class="emote-api-control-header">
-      <span>{{ name }}</span>
-    </div>
-    <div class="emote-api-control-body">
+      <div class="emote-api-control-header-provider">{{ name }}</div>
       <button class="button" @click="getEmotes()">
         <font-awesome-icon :icon="emotes.length ? 'redo' : 'download'" /><span
           class="button-text"
           >{{ emotes.length ? "Refresh" : "Download" }}</span
         >
       </button>
+    </div>
+    <div class="emote-api-control-body">
       <div class="emote-api-control-emotelist-wrapper" v-if="emotes.length">
-        <ul>
-          <li v-for="emote in emotes" v-bind:key="emote.id">
-            <img :src="emote.image" :alt="emote.name" />
-          </li>
-        </ul>
+        <div v-for="emote in emotes" v-bind:key="emote.id">
+          <img :src="emote.image" :alt="emote.name" />
+        </div>
       </div>
     </div>
   </div>
@@ -36,5 +34,23 @@ export default {
 .button-text {
   padding-left: 0.25em;
   font-variant: small-caps;
+}
+
+.emote-api-control-header {
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+
+.emote-api-control-header-provider {
+  font-size: 1.5em;
+  font-weight: bold;
+}
+
+.emote-api-control-emotelist-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-content: flex-start;
 }
 </style>
