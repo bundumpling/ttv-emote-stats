60d0363128fe4ae9c081869b5ef66753f796d648
bundumpling <bunyourdumplings@gmail.com>
2021-08-21 21:01:05 -0400

    refactor

--- src/helpers/logParser.ts
+++ src/helpers/logParser.ts
@@ -1,10 +1,6 @@
 import { IEmote } from "../types";
 
 export default async function logParser(log: string, emotes: IEmote[]) {
-
-  // Print first line of log to display date and time logging started
-  // console.log(log.slice(0, 60)); 
-
   const resultsMap = new Map<any, any>(
     emotes.map((e: IEmote, i: number) =>
       [
@@ -15,95 +11,138 @@ export default async function logParser(log: string, emotes: IEmote[]) {
           usedBy: {}
         }
       ]));
-  const blankTimestampedLineRegEx = /\[\d{2}:\d{2}:\d{2}\]\s{1}\n/;
-  const startsWithTimeStampRegEx = /\[\d{2}:\d{2}:\d{2}\]\s{2}/;
-  const timestampLength = 12;
-  let cursor = log.indexOf('\n') + 1;
+
+  let cursor = log.indexOf('\n') + 1; // get to the first line;
   let word = "";
   let username = "";
   let char = "";
 
-  const parseLine = () => {
-    if (blankTimestampedLineRegEx.test(log.slice(cursor - 12, cursor))) {
-      // console.log(`blank line detected at: '${log.slice(cursor - 12, cursor)}'`)
-      return;
-    }
-    while (log.charAt(cursor) !== ':' && cursor < log.length) {
+  const blankTimestampedLineRegEx = /\[\d{2}:\d{2}:\d{2}\]\s{1}\n/;
+  const startsWithTimeStampRegEx = /\[\d{2}:\d{2}:\d{2}\]\s{2}/;
+  const lengthOfTimeStampWithSingleSpace = 11;
+  const lengthOfTimeStampWithDoubleSpace = 12;
+
+  const isTimeStampedWithDoubleSpaceBeforeUsername = () => startsWithTimeStampRegEx.test(log.slice(cursor, cursor + 12));
+  const isBlankTimestampedLine = () => blankTimestampedLineRegEx.test(log.slice(cursor - 12, cursor));
+  const isLoggerComment = () => log.charAt(cursor) === '#';
+
+  const charIsNotNewline = () => char !== '\n';
+  const cursorIsNotAtEOF = () => cursor < log.length;
+  const cursorIsAtColon = () => log.charAt(cursor) === ':';
+  const cursorIsAtNewline = () => log.charAt(cursor) === '\n';
+  const charIsNotSpace = () => char !== ' ';
+
+  const buildUsername = () => {
+    while (!cursorIsAtColon()) {
       username += log.charAt(cursor);
       cursor++;
     }
     cursor += 2; // get past ': ' after username
-
     char = log.charAt(cursor);
-    while (char !== '\n' && cursor < log.length) {
-      while (char !== ' ' && char !== '\n') {
-        word += char;
-        cursor++;
-        char = log.charAt(cursor);
-      }
-
-      if (resultsMap.has(word)) {
-        // console.log(username, word)
-        const mapEntry = resultsMap.get(word);
-        // console.log(`Map Entry: ${JSON.stringify(mapEntry)}`)
-        mapEntry.count++;
-
-        if (mapEntry.usedBy[username] === undefined) {
-          // NOTE: This is a good check for a log being corrupted since timestamps will appear in strange places
-          // if (username.startsWith('[')) {
-          //   console.log(username, word)
-          // }
-          mapEntry.usedBy[username] = 1;
-        } else {
-          mapEntry.usedBy[username]++;
-        }
-      }
-
-      if (char === ' ') cursor++;
-      word = "";
+  }
+  const buildWord = () => {
+    while (charIsNotSpace() && charIsNotNewline() && cursorIsNotAtEOF()) {
+      word += char;
+      cursor++;
       char = log.charAt(cursor);
     }
-    username = "";
-    cursor++; // get to new line
   }
 
-  while (cursor < log.length - 1) {
-    if (!startsWithTimeStampRegEx.test(log.slice(cursor, cursor + 12))) {
-      // console.log(`--- Failed Line: '${log.slice(cursor, cursor + 20)}'`);
-      if (log.charAt(cursor) === '#') {
-        // logger message, go directly to new line
-        // console.log(`Found Logger message: ${log.slice(cursor, cursor + 20)}`)
-        while (log.charAt(cursor) !== '\n' && cursor < log.length) {
-          cursor++;
-        }
-        cursor++;
+  const parseWord = () => {
+    if (resultsMap.has(word)) {
+      // console.log(username, word)
+      const mapEntry = resultsMap.get(word);
+      // console.log(`Map Entry: ${JSON.stringify(mapEntry)}`)
+      mapEntry.count++;
+
+      if (mapEntry.usedBy[username] === undefined) {
+        // NOTE: This is a good check for a log being corrupted since timestamps will appear in strange places
+        // if (username.startsWith('[')) {
+        //   console.log(username, word)
+        // }
+        mapEntry.usedBy[username] = 1;
       } else {
-        let skipped = log.slice(cursor, cursor + 12);
-        // console.log(`Skipped: '${skipped}'`);
-        cursor += 12;
-        // console.log(`Char at +11 ${log.charAt(cursor)}`)
-        let lookForNewline = true;
-        while (lookForNewline && cursor < log.length) {
-          skipped += log.charAt(cursor);
-          cursor++;
-          if (log.charAt(cursor) === '\n') {
-            lookForNewline = false;
-            cursor++;
-            // console.log(`Skipped line: ${skipped}`)
-          } else if (log.charAt(cursor) === ':') {
-            // console.log(`Going back to process this line: '${skipped}'`)
-            cursor -= (skipped.length - 11);
-            // console.log(`Char skipped back to: '${log.charAt(cursor)}`)
-            // console.log(`Line to parse: '${log.slice(cursor, cursor + skipped.length + 1)}'`)
-            lookForNewline = false;
-            parseLine();
-          }
-        }
+        mapEntry.usedBy[username]++;
       }
-    } else {
-      cursor += timestampLength; // get past timestamp
-      parseLine();
     }
   }
+  const parseLine = () => {
+    if (isBlankTimestampedLine()) return;
+
+    buildUsername();
+
+    while (charIsNotNewline() && cursorIsNotAtEOF()) {
+      buildWord();
+      parseWord();
+      prepareForNextWord();
+    }
+
+    prepareForNextLine();
+  }
+
+  const prepareForNextWord = () => {
+    // Check whether there is another word
+    if (char === ' ') {
+      cursor++; // go to next word
+    }
+
+    word = "";
+    char = log.charAt(cursor);
+  }
+  const prepareForNextLine = () => {
+    username = "";  // reset username
+    cursor++; // go to next line
+  }
+
+  const moveToStartOfNextLine = () => {
+    while (charIsNotNewline() && cursorIsNotAtEOF()) {
+      cursor++;
+    }
+    cursor++;
+  }
+  const moveToStartOfUsername = (offset: number) => {
+    cursor += offset;
+  }
+
+  const handlePotentialUserMessage = () => {
+    // Most of the time a line like this with a single space after the timestamp is a channel status message from Chatterino.
+    // For example: "Now hosting bundumpling" or "bundumpling has gone offline. Exiting host mode."
+    // Sometimes it is a valid message in the channel from a user though.
+    // This typically happens with usernames that have internationalized language variations.
+    // For example: "アナホリシモン anahorishimon: some message"
+    // By looking for a colon (:) character we can be reasonably sure whether it's a status message to skip or a user message to parse.
+
+    let skipped = log.slice(cursor, cursor += 12);
+    let lookForNewline = true;
+    while (lookForNewline && cursorIsNotAtEOF()) {
+      skipped += log.charAt(cursor);
+      cursor++;
+      if (cursorIsAtNewline()) {
+        lookForNewline = false;
+        cursor++;
+      } else if (cursorIsAtColon()) {
+        // Found a line where the username is separated from the preceding timestamp by only a single space.
+        cursor -= skipped.length;
+        lookForNewline = false;
+        moveToStartOfUsername(lengthOfTimeStampWithSingleSpace);
+        parseLine();
+      }
+    }
+  }
+
+
+  while (cursorIsNotAtEOF()) {
+    if (isTimeStampedWithDoubleSpaceBeforeUsername()) {
+      moveToStartOfUsername(lengthOfTimeStampWithDoubleSpace); // get past timestamp
+      parseLine();
+    } else {
+      if (isLoggerComment()) {
+        moveToStartOfNextLine();
+      } else {
+        handlePotentialUserMessage();
+      }
+    }
+  }
+
   return resultsMap;
 }
