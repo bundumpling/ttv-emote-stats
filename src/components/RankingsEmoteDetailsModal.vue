4e996b3a357b0a6f3be2251f5a315024c537a38e
bundumpling <bunyourdumplings@gmail.com>
2021-08-22 23:21:09 -0400

    add top 10 users to emote details view

--- src/components/RankingsEmoteDetailsModal.vue
+++ src/components/RankingsEmoteDetailsModal.vue
@@ -11,15 +11,41 @@
       </header>
       <section class="modal-card-body">
         <div class="subheader">
-          #{{ details.rank }} in {{ details.fromList }} Rankings
+          <span class="rank">#{{ details.rank }}</span> in
+          {{ details.fromList }} Rankings
         </div>
         <div>
-          <p>Used {{ details.count }} times in {{ channelName }}'s channel.</p>
-          <p>
-            Most used by {{ mostUsedBy[0].username }} a total of
-            {{ mostUsedBy[0].count }} times!
+          <p class="subheader-usedinchannel">
+            Used <span class="count">{{ details.count }}</span> times in
+            <span class="channel">{{ channelName }}</span
+            >'s channel.
+          </p>
+          <p class="subheader-mostusedby">
+            Most used by
+            <span class="username">{{ mostUsedBy[0].username }}</span> a total
+            of <span class="count">{{ mostUsedBy[0].count }}</span> times!
           </p>
         </div>
+      </section>
+      <section class="modal-card-body mostusedby-wrapper">
+        <div class="mostusedby-header">Top 10 Users</div>
+        <ol class="mostusedby" type="none">
+          <li
+            v-for="(user, rank) in mostUsedBy.slice(0, 10)"
+            :key="user.username"
+            :class="
+              rank < 3
+                ? ['gold', 'silver', 'bronze'][rank]
+                : ['lightgray', ''][rank % 2]
+            "
+          >
+            <div class="mostusedby-rank-and-username">
+              <span class="mostusedby-rank">{{ rank + 1 }}</span>
+              <span class="mostusedby-username">{{ user.username }}</span>
+            </div>
+            <span class="mostusedby-count">{{ user.count }}</span>
+          </li>
+        </ol>
       </section>
       <footer class="modal-card-foot">
         <button class="button" @click="close()">Close</button>
@@ -69,5 +95,114 @@ export default defineComponent({
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.subheader {
+  text-align: center;
+  font-size: 1.3em;
+  .rank {
+    font-size: 1.5em;
+    font-weight: bold;
+    font-family: monospace;
+    color: black;
+  }
+}
+
+.subheader-usedinchannel {
+  text-align: center;
+  font-size: 1.2em;
+
+  .count {
+    font-size: 1.3em;
+    font-weight: bold;
+    font-family: monospace;
+    color: black;
+  }
+  .channel {
+    font-size: 1.3em;
+    font-variant: small-caps;
+    color: #111;
+  }
+}
+
+.subheader-mostusedby {
+  text-align: center;
+  font-size: 1.1em;
+
+  .username,
+  .count {
+    font-size: 1.2em;
+    font-family: monospace;
+    color: black;
+    font-weight: bold;
+  }
+}
+
+.rankings {
+  text-align: center;
+  font-size: 1.1em;
+  font-weight: bold;
+  font-variant: small-caps;
+}
+
+.mostusedby {
+  width: 50%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+
+  li {
+    display: flex;
+    justify-content: space-between;
+    font-family: monospace;
+  }
+}
+
+.mostusedby-rank-and-username {
+  display: flex;
+  justify-content: space-between;
+}
+
+.mostusedby-rank {
+  font-size: 1em;
+  min-width: 1.5em;
+  text-align: right;
+  margin-right: 0.5em;
+  font-weight: bold;
+  color: black;
+}
+
+.mostusedby-username {
+  color: black;
+}
+
+.mostusedby-count {
+  color: black;
+}
+
+.mostusedby-header {
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.3em;
+  margin-bottom: 0.5em;
+}
+
+.gold,
+.silver,
+.bronze {
+  font-weight: bold;
+}
+
+.gold {
+  background-color: #af9500;
+}
+.silver {
+  background-color: #d7d7d7;
+}
+.bronze {
+  background-color: #ad8a56;
+}
+.lightgray {
+  background-color: #eee;
+}
 </style>
