ee4515580165fdcfbbaf46f3efda163146879310
bundumpling <bunyourdumplings@gmail.com>
2021-09-01 22:10:06 -0400

    create Channel view to use state fetched from db

---
+++ src/pages/ChannelPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <TheSubheader :msg="`Channel Emote Rankings for ${channelName}`" />
+  <Controls />
+  <TabsContainer />
+  <EmoteListContainer />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import { useStore } from "../store";
+
+import TheSubheader from "../components/TheSubheader.vue";
+import Controls from "../components/RankingsControls.vue";
+import TabsContainer from "../components/RankingsTabContainer.vue";
+import EmoteListContainer from "../components/RankingsEmoteListContainer.vue";
+
+export default defineComponent({
+  name: "ChannelPage",
+  components: {
+    TheSubheader,
+    Controls,
+    TabsContainer,
+    EmoteListContainer,
+  },
+  setup() {
+    const route = useRoute();
+    const store = useStore();
+
+    const channelName = route.params.channelName;
+
+    onMounted(() => {
+      store.dispatch("fetchChannelData", channelName);
+    });
+
+    return {
+      channelName,
+    };
+  },
+});
+</script>
+
+<style>
+</style>
