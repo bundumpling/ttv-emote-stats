325c67a25ce85b0649f2a5a077a30755324c9804
bundumpling <bunyourdumplings@gmail.com>
2021-09-10 01:09:07 -0400

    add channelName param validation to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,17 @@
-import { createWebHistory, createRouter, RouteRecordRaw } from 'vue-router';
+import { createWebHistory, createRouter, RouteRecordRaw, RouteLocationNormalized, NavigationGuardNext } from 'vue-router';
+
+
+const validateChannelName = (channelName: string | string[]) =>
+  (/^[a-zA-Z0-9_]{4,25}$/.test(Array.isArray(channelName) ? channelName.join() : channelName))
+
+
+const beforeEnterRouteWithChannelName = (to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) => {
+  if (validateChannelName(to.params.channelName)) {
+    next()
+  } else {
+    next({ name: "NotFound" });
+  }
+};
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -17,9 +30,21 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import("../pages/SettingsPage.vue")
   },
   {
+    path: "/channel/:channelName/settings",
+    name: "ChannelSettings",
+    component: () => import("../pages/SettingsPage.vue"),
+    beforeEnter: beforeEnterRouteWithChannelName
+  },
+  {
     path: "/channel/:channelName",
     name: "Channel",
-    component: () => import("../pages/ChannelPage.vue")
+    component: () => import("../pages/ChannelPage.vue"),
+    beforeEnter: beforeEnterRouteWithChannelName
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: () => import("../pages/NotFoundPage.vue")
   }
 ];
 
@@ -27,5 +52,10 @@ const router = createRouter({
   history: createWebHistory(),
   routes
 });
+
+router.resolve({
+  name: 'NotFound',
+  params: { pathMatch: 'NotFound' }
+}).href
 
 export default router;
