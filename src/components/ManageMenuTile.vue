4c4fd4c8992da9c31d03e90ae1ceb29047a15203
bundumpling <bunyourdumplings@gmail.com>
2021-09-11 22:01:33 -0400

    create menu tiles for channel manage page

---
+++ src/components/ManageMenuTile.vue
@@ -0,0 +1,81 @@
+<template>
+  <div
+    class="menu-tile"
+    :class="isHoveredOver ? 'menu-tile-hovered' : ''"
+    :style="`background-image: url('/img/ManageMenuTile/${$props.backgroundPath}')`"
+    v-on:mouseover="mouseOver"
+    v-on:mouseout="mouseOut"
+  >
+    <div class="menu-tile-text">
+      <span>{{ $props.text }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "ManageMenuTile",
+  components: {},
+  props: {
+    backgroundPath: String,
+    text: String,
+  },
+  setup() {
+    const isHoveredOver = ref(false);
+
+    const mouseOver = () => {
+      isHoveredOver.value = true;
+    };
+
+    const mouseOut = () => {
+      isHoveredOver.value = false;
+    };
+    return {
+      isHoveredOver,
+      mouseOver,
+      mouseOut,
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+.menu-tile {
+  width: 284px;
+  height: 224px;
+
+  margin: 1em;
+
+  border: 8px solid #333;
+  border-radius: 8px;
+
+  filter: grayscale(60%);
+  color: #eee;
+}
+
+.menu-tile-hovered {
+  filter: none;
+  color: #f56512;
+  cursor: pointer;
+}
+
+.menu-tile-text {
+  position: relative;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+
+  span {
+    align-self: center;
+    flex-grow: 1;
+    font-size: 2.2em;
+    font-weight: bold;
+    font-variant: small-caps;
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+}
+</style>
---
+++ src/pages/ManagePage.vue
@@ -0,0 +1,54 @@
+<template>
+  <TheSubheader :msg="`Manage ${channelName}'s Channel`" />
+  <div class="container tile-wrapper">
+    <ManageMenuTile
+      v-for="(tile, index) in tiles"
+      v-bind:key="`${index}-${tile.text}`"
+      :text="tile.text"
+      :backgroundPath="tile.background"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useRoute } from "vue-router";
+import { useStore } from "../store";
+import TheSubheader from "../components/TheSubheader.vue";
+import ManageMenuTile from "../components/ManageMenuTile.vue";
+
+export default defineComponent({
+  name: "ManagePage",
+  components: {
+    TheSubheader,
+    ManageMenuTile,
+  },
+  setup() {
+    const route = useRoute();
+    const channelName = route.params.channelName;
+
+    const tiles = [
+      {
+        text: "Update Emotes",
+        background: "UpdateEmotes-background-284x224.png",
+      },
+      {
+        text: "Parse Logs",
+        background: "ParseLogs-background-284x224.png",
+      },
+    ];
+
+    return {
+      channelName,
+      tiles,
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+.tile-wrapper {
+  display: flex;
+  justify-content: center;
+}
+</style>
