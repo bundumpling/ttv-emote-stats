7fb22bdc4e407dde923724f818d8ce3225783ab3
bundumpling <bunyourdumplings@gmail.com>
2021-09-19 04:32:48 -0400

    add font-awesome window-close icon

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import App from "@/App.vue"
 import router from './router/'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUpload, faDownload, faRedo, faChevronLeft, faChevronRight, faChartBar, faCogs, faEdit, faCheck, faExpandAlt, faCompressAlt } from '@fortawesome/free-solid-svg-icons'
+import { faUpload, faDownload, faRedo, faChevronLeft, faChevronRight, faChartBar, faCogs, faEdit, faCheck, faExpandAlt, faCompressAlt, faWindowClose } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 
-library.add(faUpload, faDownload, faRedo, faChevronLeft, faChevronRight, faChartBar, faCogs, faEdit, faCheck, faExpandAlt, faCompressAlt)
+library.add(faUpload, faDownload, faRedo, faChevronLeft, faChevronRight, faChartBar, faCogs, faEdit, faCheck, faExpandAlt, faCompressAlt, faWindowClose)
 
 require('./assets/sass/main.scss');
 
