9bafd08b9eacbbe96736751bfa6c6cc5276162d2
bundumpling <bunyourdumplings@gmail.com>
2021-09-27 02:46:58 -0400

    move mongodb instance to separate file

---
+++ api_relay_server/db.js
@@ -0,0 +1,32 @@
+const { MongoClient } = require("mongodb");
+const url = "mongodb://localhost:27017";
+
+const dbName = "TTVEmoteStats";
+
+let _db = null;
+
+async function connectToDb() {
+  return new Promise((resolve, reject) => {
+    if (_db) resolve(_db);
+
+    MongoClient.connect(url, (err, client) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        _db = client.db(dbName);
+        console.log("Successfully connected to MongoDB");
+        resolve(_db);
+      }
+    });
+  });
+}
+
+function getDb() {
+  return _db;
+}
+
+module.exports = {
+  connectToDb,
+  getDb,
+};
