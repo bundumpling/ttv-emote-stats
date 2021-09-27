0f61362bedc7a36d24abfe7ca3f9ea4fc5def7a2
bundumpling <bunyourdumplings@gmail.com>
2021-09-27 06:55:47 -0400

    remove obsolete vuex store code

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,26 +1,9 @@
 // import { InjectionKey } from "@vue/runtime-core";
 import { createStore, Store as VuexStore, CommitOptions } from "vuex";
 import { Mutations, mutations, MutationType } from "./mutations";
-import { Emote, ILogParserResults } from "../types"
+import { Emote, LogParserResults } from "../types"
 
 export interface State {
-  channel: {
-    name: string,
-    twitchID: string,
-    emotes: Array<Emote>,
-    hasEmotesFrom: {
-      'Twitch': boolean,
-      'FFZ': boolean,
-      'BTTV': boolean,
-      '7TV': boolean
-    },
-    emoteDetails: any,
-    emoteDetailsModalOpen: boolean,
-    emotesPerPage: number,
-    emoteSearchInput: string,
-    userSearchInput: string,
-    userSearchLock: boolean
-  },
   settings: {
     channelEmoteData: {
       channelID: string,
@@ -28,31 +11,12 @@ export interface State {
       emotesFromDatabase: object[],
       emotesFromProviders: object[],
       emoteCodes: string[]
-    },
-    logParserResults: ILogParserResults,
-    logParserFilenames: string[],
+    }
   }
 }
 
 export const store = createStore<State>({
   state: {
-    channel: {
-      name: 'bundumpling',
-      twitchID: "472309577",
-      emotes: [],
-      hasEmotesFrom: {
-        'Twitch': false,
-        'FFZ': false,
-        'BTTV': false,
-        '7TV': false
-      },
-      emoteDetailsModalOpen: false,
-      emoteDetails: {},
-      emotesPerPage: 10,
-      emoteSearchInput: '',
-      userSearchInput: '',
-      userSearchLock: false
-    },
     settings: {
       channelEmoteData: {
         channelName: '',
@@ -60,45 +24,11 @@ export const store = createStore<State>({
         emotesFromDatabase: [],
         emotesFromProviders: [],
         emoteCodes: []
-      },
-      logParserResults: {},
-      logParserFilenames: [],
+      }
     }
   },
   mutations,
   actions: {
-    fetchChannelEmoteCounts({ state, commit }, channelName: string) {
-      const URL = `http://localhost:8081/channel/${channelName}/emoteCounts`;
-      fetch(URL, {
-        method: 'GET',
-        headers: {
-          'content-type': 'application/json'
-        }
-      })
-        .then(res => res.json())
-        .then(({ _id, emotes }) => {
-          const twitchID = _id;
-          type thasEmotesFrom = {
-            [key: string]: boolean
-          }
-          const hasEmotesFrom = ['Twitch', 'FFZ', 'BTTV', '7TV'].reduce<thasEmotesFrom>((result, provider) => ({
-            [provider]: emotes.some((emote: { provider: string; }) => emote.provider === provider),
-            ...result
-          }), {})
-
-          commit(MutationType.SetChannelData, {
-            ...state.channel,
-            name: channelName,
-            twitchID,
-            emotes,
-            hasEmotesFrom
-          })
-        })
-        .catch(err => {
-          console.error(err);
-        })
-
-    },
     getChannelEmoteCodes({ state, commit }, channelName) {
       const URL = `http://localhost:8081/channel/${channelName}/emoteCodes`;
       fetch(URL, {
@@ -115,29 +45,6 @@ export const store = createStore<State>({
             emoteCodes
           });
         })
-    },
-    async fetchEmoteUsageDetailsForChannelPage({ state, commit }, { emote, emoteListProvider }) {
-      if (state.channel.emotes[emote.stateIndex].usedBy !== undefined && state.channel.emotes[emote.stateIndex].usedOn !== undefined) {
-        commit(MutationType.OpenEmoteDetailsModal, {
-          emote,
-          fromList: emoteListProvider
-        })
-      } else {
-        const URL = `http://localhost:8081/emote/${emote._id}/usageDetails`;
-        fetch(URL, {
-          method: 'GET',
-          headers: {
-            'content-type': 'application/json'
-          }
-        })
-          .then(res => res.json())
-          .then(({ usedBy, usedOn }) => {
-            commit(MutationType.OpenEmoteDetailsModal, {
-              emote: { usedBy, usedOn, ...emote },
-              fromList: emoteListProvider,
-            });
-          });
-        }
     },
     async getChannelEmotesFromDatabaseAndProviders({ state, commit }, channelName) {
       const URL = `http://localhost:8081/channel/${channelName}/getChannelEmotesFromDatabaseAndProviders`
@@ -172,31 +79,6 @@ export const store = createStore<State>({
         }
       })
     },
-    saveLogParserResultsToDB({ state, commit }) {
-      const channelName = state.settings.channelEmoteData.channelName;
-      const logFilenames = state.settings.logParserFilenames;
-      const logParserResults = state.settings.logParserResults;
-
-      const URL = `http://localhost:8081/channel/${channelName}/updateCountsFromLog`;
-
-      fetch(
-        URL,
-        {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            logFilenames,
-            logParserResults
-          })
-        }
-      ).then(response => response.json()
-      ).then(json => {
-        // Need to do anything here?
-      });
-    }
   }
 })
 
