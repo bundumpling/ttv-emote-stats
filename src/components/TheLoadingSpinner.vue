a9674da5d7b5363e1f4faeb556747017710e49f5
bundumpling <bunyourdumplings@gmail.com>
2021-09-27 09:26:14 -0400

    change loading spinner to fixed centered

--- src/components/TheLoadingSpinner.vue
+++ src/components/TheLoadingSpinner.vue
@@ -14,14 +14,10 @@ export default {
 
 <style lang="scss" scoped>
 .container {
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 
 .spinner {
