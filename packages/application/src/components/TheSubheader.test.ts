5ac40aa4c9e9c1e4e9f7025c81dab307803a79ea
bundumpling <bunyourdumplings@gmail.com>
2021-10-07 02:16:23 -0400

    add test for TheSubheader component

---
+++ packages/application/src/components/TheSubheader.test.ts
@@ -0,0 +1,11 @@
+import { mount } from '@vue/test-utils';
+import TheSubheader from './TheSubheader.vue';
+
+describe('TheSubheader', () => {
+  it('should display header text', () => {
+    const msg = "I Am Some Subheader Text";
+    const wrapper = mount(TheSubheader, { props: { msg } });
+
+    expect(wrapper.find('h2').text()).toEqual(msg);
+  })
+})
