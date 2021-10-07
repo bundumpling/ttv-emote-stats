0d6cdf67b954e1247c003a211b4d0df5b28ab759
bundumpling <bunyourdumplings@gmail.com>
2021-10-07 03:08:06 -0400

    relocate and rename unit test file

--- packages/application/tests/unit/TheSubheader.spec.ts
+++ packages/application/tests/unit/TheSubheader.spec.ts
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils';
-import TheSubheader from './TheSubheader.vue';
+import TheSubheader from '@/components/TheSubheader.vue';
 
 describe('TheSubheader', () => {
   it('should display header text', () => {
