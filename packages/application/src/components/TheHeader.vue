1da90e29ee0a25c2dcd68334e78fd7f5330bdfdd
bundumpling <bunyourdumplings@gmail.com>
2021-10-20 20:09:30 -0400

    restyle with css utility classes

--- packages/application/src/components/TheHeader.vue
+++ packages/application/src/components/TheHeader.vue
@@ -1,9 +1,9 @@
 <template>
-  <figure class="container" role="img" aria-labelledby="TTV EMOTE STATS LOGO">
+  <figure class="w-full" role="img" aria-labelledby="TTV EMOTE STATS LOGO">
     <router-link to="/"
       ><img
         src="/img/logo.png"
-        class="ascii-art-banner"
+        class="pt-1 mb-8 w-full"
         alt="ASCII block art logo for TTV Emote Stats"
       />
     </router-link>
@@ -16,10 +16,3 @@ export default defineComponent({
   name: "TheHeader",
 });
 </script>
-
-<style scoped>
-.ascii-art-banner {
-  padding-top: 0.75em;
-  margin-bottom: 2em;
-}
-</style>
--- packages/application/src/components/TheSubheader.vue
+++ packages/application/src/components/TheSubheader.vue
@@ -1,8 +1,12 @@
 <template>
-  <h2>{{ msg }}</h2>
+  <h2
+    class="py-4 font-inconsolata text-4xl font-medium tracking-wide text-center"
+  >
+    {{ msg }}
+  </h2>
 </template>
 
-<script>
+<script lang="ts">
 export default {
   name: "TheSubheader",
   props: {
@@ -13,14 +17,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped>
-@import url("https://fonts.googleapis.com/css2?family=Inconsolata:wght@500&display=swap");
-h2 {
-  font-family: "Inconsolata", monospace;
-  padding-bottom: 0.75em;
-  font-size: 2.2em;
-  font-weight: 500;
-  text-align: center;
-}
-</style>
