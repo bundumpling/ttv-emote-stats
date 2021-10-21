385d7f6892af44908d3a85005c0497d6fa17148e
bundumpling <bunyourdumplings@gmail.com>
2021-10-21 06:29:27 -0400

    style with css utility classes

--- packages/application/src/components/TheLoadingSpinner.vue
+++ packages/application/src/components/TheLoadingSpinner.vue
@@ -1,38 +1,15 @@
 <template>
-  <div class="container">
-    <div class="spinner">
-      <font-awesome-icon icon="spinner" class="spinner" />
+  <div
+    class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+  >
+    <div class="text-center">
+      <font-awesome-icon icon="spinner" class="text-7xl animate-spin" />
     </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 export default {
   name: "TheLoadingSpinner",
 };
 </script>
-
-<style lang="scss" scoped>
-.container {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-
-.spinner {
-  font-size: 3em;
-  animation-name: spin;
-  animation-duration: 2000ms;
-  animation-iteration-count: infinite;
-  animation-timing-function: linear;
-}
-@keyframes spin {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(359deg);
-  }
-}
-</style>
