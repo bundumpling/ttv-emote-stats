77afa88a2763ac3c037bdee64e60b5333a8b394d
bundumpling <bunyourdumplings@gmail.com>
2021-10-22 15:17:51 -0400

    add text shadow

--- packages/application/src/components/TheSubheader.vue
+++ packages/application/src/components/TheSubheader.vue
@@ -1,6 +1,13 @@
 <template>
   <h2
-    class="py-5 font-inconsolata text-4xl font-medium tracking-wide text-center"
+    class="
+      py-5
+      font-inconsolata
+      text-4xl
+      font-medium
+      tracking-wide
+      text-center text-shadow-sm
+    "
   >
     {{ msg }}
   </h2>
