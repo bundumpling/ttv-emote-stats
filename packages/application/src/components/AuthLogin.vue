3ae79f27b5c7a7ab674596072fa108acc21e1328
bundumpling <bunyourdumplings@gmail.com>
2021-10-28 16:39:11 -0400

    restyle with css utility classes

--- packages/application/src/components/AuthLogin.vue
+++ packages/application/src/components/AuthLogin.vue
@@ -1,32 +1,80 @@
 <template>
-  <div class="container box">
-    <h2 class="header">Log In</h2>
-    <form class="form" @submit.prevent="loginUser">
-      <div class="field">
-        <label class="label">Username</label>
-        <div class="control">
-          <input
-            id="username"
-            v-model="login.username"
-            type="text"
-            class="input"
-            aria-label="username"
-          />
-        </div>
+  <div
+    class="
+      my-4
+      mx-auto
+      p-2
+      w-1/2
+      bg-light-50
+      border-4 border-gray-200
+      rounded-lg
+      list-none
+      shadow-indigo-400 shadow-lg
+    "
+  >
+    <h2
+      class="
+        p-2
+        text-2xl text-center
+        tracking-wide
+        font-bold
+        text-dark-100 text-shadow-sm
+      "
+    >
+      User Login
+    </h2>
+    <form @submit.prevent="loginUser">
+      <label class="px-1 text-md small-caps font-bold">Username</label>
+      <input
+        id="username"
+        v-model="login.username"
+        type="text"
+        class="
+          my-1
+          max-w-full
+          w-full
+          p-2
+          shadow-inner
+          border-cool-gray-300 border-2
+          rounded-md
+        "
+        aria-label="username"
+      />
+      <label class="px-1 text-md small-caps font-bold">Password</label>
+      <input
+        id="password"
+        v-model="login.password"
+        type="password"
+        class="
+          my-1
+          max-w-full
+          w-full
+          p-2
+          shadow-inner
+          border-cool-gray-300 border-2
+          rounded-md
+        "
+        aria-label="password"
+      />
+      <div class="mt-3 mb-2 flex justify-center align-baseline">
+        <button
+          class="
+            bg-blue-500
+            text-light-200
+            tracking-wider
+            px-4
+            py-1
+            border-2
+            font-bold
+            text-lg
+            rounded-md
+            shadow-md shadow-cyan-800
+          "
+          type="submit"
+        >
+          Login
+        </button>
       </div>
-      <div class="field">
-        <label class="label">Password</label>
-        <div class="control">
-          <input
-            id="password"
-            v-model="login.password"
-            type="password"
-            class="input"
-            aria-label="password"
-          />
-        </div>
-      </div>
-      <button class="button" type="submit">Login</button>
     </form>
   </div>
 </template>
@@ -69,19 +117,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style lang="scss" scoped>
-.container {
-  width: 50%;
-}
-.header {
-  text-align: center;
-  font-size: 2em;
-  font-variant: small-caps;
-  font-weight: bold;
-}
-.label {
-  font-variant: small-caps;
-  font-weight: bold;
-}
-</style>
--- packages/application/src/components/AuthRegister.vue
+++ packages/application/src/components/AuthRegister.vue
@@ -1,30 +1,82 @@
 <template>
-  <div class="container box">
-    <h2 class="header">Sign Up</h2>
-    <form class="form" @submit.prevent="registerUser">
-      <div class="field">
-        <label class="label">Username</label>
-        <input
-          id="username"
-          v-model="register.username"
-          type="text"
-          class="input"
-          aria-label="Username"
-          required
-        />
+  <div
+    class="
+      my-4
+      mx-auto
+      p-2
+      w-1/2
+      bg-light-50
+      border-4 border-gray-200
+      rounded-lg
+      list-none
+      shadow-indigo-400 shadow-lg
+    "
+  >
+    <h2
+      class="
+        p-2
+        text-2xl text-center
+        tracking-wide
+        font-bold
+        text-dark-100 text-shadow-sm
+      "
+    >
+      Sign Up
+    </h2>
+    <form @submit.prevent="registerUser">
+      <label class="px-1 text-md small-caps font-bold">Username</label>
+      <input
+        id="username"
+        v-model="register.username"
+        type="text"
+        class="
+          my-1
+          max-w-full
+          w-full
+          p-2
+          shadow-inner
+          border-cool-gray-300 border-2
+          rounded-md
+        "
+        aria-label="Username"
+        required
+      />
+      <label class="px-1 text-md small-caps font-bold">Password</label>
+      <input
+        id="password"
+        v-model="register.password"
+        type="password"
+        class="
+          my-1
+          max-w-full
+          w-full
+          p-2
+          shadow-inner
+          border-cool-gray-300 border-2
+          rounded-md
+        "
+        aria-label="Password"
+        required
+      />
+      <div class="mt-3 mb-2 flex justify-center align-baseline">
+        <button
+          class="
+            bg-blue-500
+            text-light-200
+            tracking-wider
+            px-4
+            py-1
+            border-2
+            font-bold
+            text-lg
+            rounded-md
+            shadow-md shadow-cyan-800
+          "
+          type="submit"
+        >
+          Register
+        </button>
       </div>
-      <div class="field">
-        <label class="label">Password</label>
-        <input
-          id="password"
-          v-model="register.password"
-          type="password"
-          class="input"
-          aria-label="Password"
-          required
-        />
-      </div>
-      <button class="button" type="submit">Register</button>
     </form>
   </div>
 </template>
@@ -56,7 +108,7 @@ export default defineComponent({
           router.push("/login");
         }
       } catch (error) {
-        console.log(error.response);
+        console.error(error);
       }
     }
 
@@ -67,19 +119,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style lang="scss" scoped>
-.container {
-  width: 50%;
-}
-.header {
-  text-align: center;
-  font-size: 2em;
-  font-variant: small-caps;
-  font-weight: bold;
-}
-.label {
-  font-variant: small-caps;
-  font-weight: bold;
-}
-</style>
