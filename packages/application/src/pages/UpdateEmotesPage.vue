5d1fc105dd9f6971427615f7716dd07efff08033
bundumpling <bunyourdumplings@gmail.com>
2021-11-29 00:12:56 -0500

    add TwitchUser prop to child component, remove obsolete function

--- packages/application/src/pages/UpdateEmotesPage.vue
+++ packages/application/src/pages/UpdateEmotesPage.vue
@@ -8,6 +8,7 @@
       :emotes-from-providers="EmotesFromProviders"
     />
     <UpdatedEmotesSummary
+      :twitch-user="TwitchUser"
       :emotes-from-providers="EmotesFromProviders"
       :emotes-from-database="EmotesFromDatabase"
     />
@@ -78,119 +79,6 @@ export default defineComponent({
     }
 
     onBeforeMount(fetchData);
-
-    // const updatedEmotes = computed((): Array<EmoteForUpdate> => {
-    //   type tResult = {
-    //     [key: string]: EmoteForUpdate;
-    //   };
-    //   let result: tResult = {};
-
-    //   databaseEmoteState.data.forEach((emote: Emote) => {
-    //     const { code, image, provider, providerID, obsolete } = emote;
-    //     result[code] = {
-    //       code,
-    //       image,
-    //       provider,
-    //       providerID,
-    //       obsolete,
-    //       isUnavailable: true,
-    //       isNew: false,
-    //       isUpdated: false,
-    //     }; // flag obsolete until matching provider emote found
-    //   });
-
-    //   providerEmoteState.data.forEach((emote: EmoteFromProvider) => {
-    //     const { code, image, provider, providerID } = emote;
-
-    //     if (!result[code]) {
-    //       // NEW EMOTE
-    //       result[code] = {
-    //         code,
-    //         image,
-    //         provider,
-    //         providerID,
-    //         obsolete: false,
-    //         isNew: true,
-    //         isUnavailable: false,
-    //         isUpdated: false,
-    //       };
-    //     } else {
-    //       // Compare...
-    //       if (
-    //         result[code].image !== image ||
-    //         result[code].provider !== provider ||
-    //         result[code].providerID !== providerID
-    //       ) {
-    //         result[code] = {
-    //           code,
-    //           image,
-    //           provider,
-    //           providerID,
-    //           obsolete: false,
-    //           isUpdated: true,
-    //           isNew: false,
-    //           isUnavailable: false,
-    //         };
-    //       } else {
-    //         result[code] = {
-    //           ...result[code],
-    //           obsolete: false,
-    //           isUpdated: false,
-    //           isUnavailable: false,
-    //           isNew: false,
-    //         };
-    //       }
-    //     }
-    //   });
-
-    //   const quantifyCompare = (emote: EmoteForUpdate) => {
-    //     const { isNew, isUnavailable, isUpdated } = emote;
-    //     let value = 0;
-    //     if (isNew) value += 5;
-    //     if (isUnavailable) value += 3;
-    //     if (isUpdated) value += 1;
-
-    //     if (value) hasUpdates.value = true;
-
-    //     return value;
-    //   };
-
-    //   return Object.keys(result)
-    //     .map((code) => ({ ...result[code] }))
-    //     .sort((a: EmoteForUpdate, b: EmoteForUpdate) => {
-    //       return quantifyCompare(b) - quantifyCompare(a);
-    //     });
-    // });
-    // async function saveUpdatedEmotes() {
-    //   const emotes = updatedEmotes.value
-    //     .filter((emote: EmoteForUpdate) => {
-    //       return emote.isNew || emote.isUnavailable || emote.isUpdated;
-    //     })
-    //     .map((emote: EmoteForUpdate) => {
-    //       if (emote.isUnavailable) {
-    //         return { ...emote, obsolete: true };
-    //       } else {
-    //         return emote;
-    //       }
-    //     });
-
-    //   try {
-    //     const URL = `http://localhost:8081/channel/${channelName}/saveUpdatedEmotes`;
-    //     const token = localStorage.getItem("user");
-    //     const requestBody = {
-    //       channelID: state.channelID,
-    //       emotes,
-    //     };
-    //     const requestHeaders = {
-    //       headers: {
-    //         authorization: token,
-    //       },
-    //     };
-    //     await axios.post(URL, requestBody, requestHeaders);
-    //   } catch (err) {
-    //     console.log(err);
-    //   }
-    // }
 
     return {
       channelName,
